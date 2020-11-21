ba69f2acb2f9f94d5638f4f0bfd76f1ef465efb0
Anderson Santos Da Silva Filho <50058700+AndersPotato@users.noreply.github.com>
2020-11-21 18:42:27 -0300

    Update final  (Espero)

--- src/files/Rust/QuizA/js/question.js
+++ src/files/Rust/QuizA/js/question.js
@@ -1,28 +1,28 @@
 const quiz = [
 {
-	q: 'Indique os respectivos tipos da seguinte operação:<br><br>let x = 32;<br> let y: i8 = -56;<br> let z = 34.5;',
-	options: ['i32, i8, f64', 'i64, u8, f32', 'u32, i8, f64', 'i32, i8, f32'],
+	q: 'let a = 20;<br>let b = a - 12;<br>let c = a * b<br><br>Informe o valor de c:',
+	options: ['160', '145', '120', '108'],
 	answer:0
 },
 {
-	q: 'Indique o tipo de variável a seguir:<br><br>let a = (3, 2.2, "macarrão", false);',
-	options: ['Array', 'String', 'Tupla', 'Boleano'],
-	answer:2
+	q: 'let a = 22;<br>let b = 32;<br> let c = a ==b;<br><br>C é um booleano com valor:',
+	options: ['falso', 'verdadeiro'],
+	answer:0
 },
 {
-	q: 'Indique o tipo de variável a seguir:<br><br>let a: [i32; 5] = [3, 2, 33, 858, 129];',
-	options: ['String', 'Array', 'Tupla', 'Boleano'],
+	q: 'let mut a = 5;<br> a+= 12;<br> a-=7;<br><br>Informe o valor final de A:',
+	options: ['17', '10', '15', '12'],
 	answer:1
 },
 {
-	q: 'Indique os respectivos tipos da seguinte operação:<br><br>let x = 88;<br> let y = "Olá mundo";<br> let f = true;',
-	options: ['i32, str, Boleano', 'u8, char, Boleano','i8, str, Tupla','i32, char, Boleano' ],
+	q: 'let mut z = 32;<br> z+= 18;<br> z*=2;<br><br>Informe o valor final de Z:',
+	options: ['100', '50','75','32' ],
 	answer:0
 },
 {
-	q: 'Indique os respectivos tipos desta Tupla:<br><br> let a = (4.3, "biscoitinho", -235, true)',
-	options: ['f32, char, u64, Boleano','f32, str, u32, Boleano', 'f64, str, i32, Boleano', 'f64, char, u32, Boleano'],
-	answer:2
+	q: 'let a = 35; <br> let b = (a as f32)/2.0;<br><br> Informe o resultao de B:',
+	options: ['17.5','17', '15.5', '19'],
+	answer:0
 }
 
 ]
--- src/files/Rust/QuizD/js/question.js
+++ src/files/Rust/QuizD/js/question.js
@@ -1,28 +1,8 @@
 const quiz = [
 {
-	q: 'Indique os respectivos tipos da seguinte operação:<br><br>let x = 32;<br> let y: i8 = -56;<br> let z = 34.5;',
-	options: ['i32, i8, f64', 'i64, u8, f32', 'u32, i8, f64', 'i32, i8, f32'],
+	q: 'Questão em branco',
+	options: ['Branco','Preto', 'Verde', 'Azul'],
 	answer:0
-},
-{
-	q: 'Indique o tipo de variável a seguir:<br><br>let a = (3, 2.2, "macarrão", false);',
-	options: ['Array', 'String', 'Tupla', 'Boleano'],
-	answer:2
-},
-{
-	q: 'Indique o tipo de variável a seguir:<br><br>let a: [i32; 5] = [3, 2, 33, 858, 129];',
-	options: ['String', 'Array', 'Tupla', 'Boleano'],
-	answer:1
-},
-{
-	q: 'Indique os respectivos tipos da seguinte operação:<br><br>let x = 88;<br> let y = "Olá mundo";<br> let f = true;',
-	options: ['i32, str, Boleano', 'u8, char, Boleano','i8, str, Tupla','i32, char, Boleano' ],
-	answer:0
-},
-{
-	q: 'Indique os respectivos tipos desta Tupla:<br><br> let a = (4.3, "biscoitinho", -235, true)',
-	options: ['f32, char, u64, Boleano','f32, str, u32, Boleano', 'f64, str, i32, Boleano', 'f64, char, u32, Boleano'],
-	answer:2
 }
 
 ]
